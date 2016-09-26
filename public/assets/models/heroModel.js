d4473b74739c501aebef5350aa963054a020fb87
Ian <shiny@what.local>
2016-09-26 14:22:59 -0500

    hero model

---
+++ public/assets/models/heroModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var heroSchema = new Schema({
+  alias: {type: String, required: true},
+  first_name: {type: String},
+  last_name: {type: String},
+  city: {type: String},
+  power_name: {type: String, enum: ['Invisibility', 'Flight', 'Super Speed', 'Heat Vision', 'Super Strength', 'Accelerated Healing', 'Power Blast', 'Animal Affinity']}
+});
+
+var heroModel = mongoose.model('heroes', heroSchema);
+module.exports = heroModel;
